1abdcca89099065d1ca17ec21e251918839fd2e2
Ali Karimi <ajkarimi7@gmail.com>
2019-09-18 22:33:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,14 @@ class Song
     song.artist_name = artist_name
     song 
   end 
-end
 
-def self.create_from_filename(filename)
-  pa
+
+  def self.create_from_filename(filename)
+        parts = filename.split(" - ")
+      artist_name = parts[0]
+      song_name = parts[1].gsub(".mp3", "")
+  
+      song = self.create
+      song.name = song_name
+      song.artist_name = artist_name
+      song
